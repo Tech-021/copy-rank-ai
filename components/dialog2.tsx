65a5a90a4f5dabaa4493c80309a850a72c9aa54b
Abdullah <ab1212123132@gmail.com>
2025-12-19 16:32:14 +0500

    add interface to dialog 2

--- components/dialog2.tsx
+++ components/dialog2.tsx
@@ -1,7 +1,22 @@
-export function CreatePostDialog(){
+"use client"
+
+import { Dialog, DialogContent, DialogTitle } from "./ui/dialog"
+
+interface CreatePostDialogProps {
+    open: boolean
+    onOpenChange: (open: boolean) => void;
+}
+
+export function CreatePostDialog({ open, onOpenChange }: CreatePostDialogProps){
     return (
         <div>
-            
+           <Dialog>
+            <DialogContent>
+                <DialogTitle>
+                    <h1 className="">Choose what to write about</h1>
+                </DialogTitle>
+            </DialogContent>
+            </Dialog> 
         </div>
     )
 }
