8a2a824ad6ecd2db0a989294d7399af42c175fb7
Abdul Rehman <rehman00004khan@gmail.com>
2025-12-26 21:24:33 +0500

    added loader everywhere

--- components/tabs/competitors-tab.tsx
+++ components/tabs/competitors-tab.tsx
@@ -917,7 +917,7 @@ export function CompetitorsTab({
               <Button
                 onClick={handleAddCompetitor}
                 variant="outline"
-                className="gap-2 text-[#53F870] border border-[#53f870]! lg:border-gray-700! rounded-r-none sm:rounded-r-none cursor-pointer bg-[rgba(83,248,112,0.1)]! hover:bg-[rgba(83,248,112,0.2)] text-xs sm:text-sm"
+                className="gap-2 text-[#53F870] border border-[#53f870]! rounded-r-none sm:rounded-r-none cursor-pointer bg-[rgba(83,248,112,0.1)]! hover:bg-[rgba(83,248,112,0.2)] text-xs sm:text-sm"
               >
                 Add Competitors
                 <Plus className="w-3 h-3 sm:w-4 sm:h-4" />
